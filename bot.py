5c2685a3d374225dd1298bd3646c748355f097da
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-06-01 12:05:30 +0300

    init

---
+++ bot.py
@@ -0,0 +1,53 @@
+import telebot
+from main import main
+
+
+bot = telebot.TeleBot('1207374644:AAFcBG7zPPIokte8LmQgru2cebBn-yWdnm8')
+chat_id = ''
+
+# Настройки прокси
+# from telebot import apihelper
+# ip = '163.172.189.32'
+# port = '8811'
+# apihelper.proxy = {
+#   'https': f'http://{ip}:{port}'
+# }
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Стас, я тупой бот, но уже кое что умею.'
+                          'Напиши /help')
+    global chat_id
+    chat_id = message.chat.id
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, напиши /help?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Просто загрузи сюда файл в формате .xlsx.\n"
+                                               "Кошельки должны быть перечислены в первом столбце. "
+                                               "Заголовок столбцу не нужен.'")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+@bot.message_handler(content_types=['document'])
+def get_fuck_you(file):
+    # print(file.document.file_id)  # строка для дебаггинга
+    file_info = bot.get_file(file.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    bot.send_message(file.from_user.id, "Fuck the System!")
+    bot.send_message(file.from_user.id, "Сервер обрабатывает запрос. возможно займет минуту - две")
+    with open('wallets.xlsx', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    main('wallets.xlsx')
+
+    doc = open('result.xlsx', 'rb')
+    bot.send_document(chat_id, doc)
+
+
+bot.polling(none_stop=True, interval=1)
---
+++ main.py
@@ -0,0 +1,73 @@
+import time
+import requests
+import openpyxl as opx
+
+
+def get_wallets(path_to_file):
+    workbook = opx.load_workbook(path_to_file)
+    sheet = workbook.worksheets[0]
+    result = []
+    for row in sheet.rows:
+        result.append(row[0].value)
+    return result
+
+
+def get_response(wallet):
+    response = requests.get('https://blockchair.com/ru/search', params={
+        'q': wallet,
+    })
+
+    if response.status_code != 200:
+        time.sleep(1)
+        response = requests.get('https://blockchair.com/ru/search', params={
+            'q': wallet,
+        })
+    elif response.status_code != 200:
+        raise SystemError
+
+    return response
+
+
+def write_to_file(data, summa, path_to_file):
+    wb = opx.Workbook()
+    ws = wb.create_sheet('Биткоины', 0)
+    ws.cell(row=1, column=1).value = 'Номер кошелька'
+    ws.cell(row=1, column=2).value = 'Количество битков'
+
+    i = 2
+    for k, v in data.items():
+        ws.cell(row=i, column=1).value = k
+        ws.cell(row=i, column=2).value = v
+        i += 1
+
+    ws.cell(row=i, column=1).value = 'Итого'
+    ws.cell(row=i, column=2).value = summa
+    wb.save(path_to_file)
+
+
+def main(data_file):
+    wallets = get_wallets(data_file)
+    wallets_with_coin = {}
+    summa = 0
+
+    for wallet in wallets:
+        response = get_response(wallet)
+        try:
+            coin = response.text[response.text.find('Баланс: <span><span>') + 20:
+                                 response.text.find('Баланс: <span><span>') + 100]
+            coin_int = coin.split('<span')[0]
+            coin_fract = coin.split('>')[1].split('</s')[0]
+            coin_num = float(coin_int) + float(coin_fract)
+        except IndexError:
+            print('Нет такого кошелька')
+            coin_num = 0
+        # pprint(response.text)  # строка для дебаггинга
+        # print(f'\n\n\n{coin}')  # строка для дебаггинга
+        wallets_with_coin[wallet] = coin_num
+        print(f'{wallet}: {coin_num} BTC')  # убрать строку
+        summa += coin_num
+    write_to_file(wallets_with_coin, summa, 'result.xlsx')
+
+
+if __name__ == '__main__':
+    main('wallets.xlsx')
